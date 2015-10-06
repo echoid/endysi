3aae06e1afed2cfb4981cc0619eb270c299bcbf3
echoid <mgmacleod@posteo.de>
2015-10-05 23:10:48 -0400

    Added basic script for generating figs for the paper

---
+++ paper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+###############################################################################
+# This script runs the experiments and generates the figures for the paper
+# to be published.
+#
+###############################################################################
+
+import endysi
+import parameters as params
+
+
+def figure1():
+    pass
